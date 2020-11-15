09e282da1aed64defe2e483c87a950b069d6edf5
Chase Harrington <acechaseharrington@gmail.com>
2020-11-14 20:46:23 -0600

    started readme file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Day-Planner
+
+## Pictures of the planner!
+![Screenshot of the planner]()
+![Screenshot of the planner]()
+
+## Github pages 
+ 
+
+## Langauges Used
+JAVASCRIPT
+CSS
+
+## How to use the planner
--- assets/script.js
+++ assets/script.js
@@ -20,47 +20,47 @@ $(document).ready(function(){
 var hours = [{
     id:"text-9",
     time: 9,
-    description: 'user-entered event',
+    description: '',
     status: 'present'
   },{
     id:"text-10",
     time: 10,
-    description: 'user-entered event',
+    description: '',
     status: 'present'
   },{
     id:"text-11",
     time: 11,
-    description: 'user-entered event',
+    description: '',
     status: 'present'
   },{
     id:"text-12",
     time: 12,
-    description: 'user-entered event',
+    description: '',
     status: 'present'
   },{
     id:"text-1",
     time: 13,
-    description: 'user-entered event',
+    description: '',
     status: 'present'
   },{
     id:"text-2",
     time: 14,
-    description: 'user-entered event',
+    description: '',
     status: 'present'
   },{
     id:"text-3",
     time: 15,
-    description: 'user-entered event',
+    description: '',
     status: 'present'
   },{
     id:"text-4",
     time: 16,
-    description: 'user-entered event',
+    description: '',
     status: 'present'
   },{
     id:"text-5",
     time: 17,
-    description: 'user-entered event',
+    description: '',
     status: 'present'
   }];
 
@@ -93,7 +93,11 @@ function onLoad() {
         else if(currentHour < hour.time) {
             hourElt.addClass("future")
         }
+
+
     })
+
+
 
 
 }
